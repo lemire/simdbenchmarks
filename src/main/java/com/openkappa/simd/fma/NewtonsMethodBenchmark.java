a927dd5c133ed61274937d95eb38a8bd40b4d314
Richard Startin <richard@openkappa.co.uk>
2017-07-17 22:25:35 +0100

    apparently these were never staged

--- src/main/java/com/openkappa/simd/fma/NewtonsMethodBenchmark.java
+++ src/main/java/com/openkappa/simd/fma/NewtonsMethodBenchmark.java
@@ -1,7 +1,22 @@
 package com.openkappa.simd.fma;
 
-/**
- * Created by richa on 16/07/2017.
- */
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class NewtonsMethodBenchmark {
+
+    @Benchmark
+    public void NM_HandWritten(Polynomial polynomial, Blackhole bh) {
+        bh.consume(new NewtonsMethod(polynomial.coefficients).solve(polynomial.initial, polynomial.maxIterations));
+    }
+
+
+    @Benchmark
+    public void NM_FMA(Polynomial polynomial, Blackhole bh) {
+        bh.consume(new NewtonsMethodFMA(polynomial.coefficients).solve(polynomial.initial, polynomial.maxIterations));
+    }
 }
--- src/main/java/com/openkappa/simd/fma/Polynomial.java
+++ src/main/java/com/openkappa/simd/fma/Polynomial.java
@@ -1,7 +1,21 @@
 package com.openkappa.simd.fma;
 
-/**
- * Created by richa on 16/07/2017.
- */
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.Random;
+
+
+@State(Scope.Thread)
 public class Polynomial {
+    double[] coefficients;
+    double initial;
+    @Param({"100", "1000", "10000"})
+    int maxIterations;
+
+    @Setup(Level.Trial)
+    public void init() {
+        this.coefficients = new double[] {4, -12, 9};
+        this.initial = new Random(0).nextDouble();
+    }
 }
